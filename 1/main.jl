ab35a81de23e7dec3e31693d9b4fe732c82e23fd
JoabFelippx <80433162+JoabFelippx@users.noreply.github.com>
2024-07-31 12:22:11 +0000

    zero of a function

--- 1/main.jl
+++ 1/main.jl
@@ -5,3 +5,36 @@ f(x) = x^2 + x + 1
 
 z = f(2)
 println(z)
+
+function fx(x, a, b, c)
+  y(n) = a*n^2 + b*n + c
+  return y(x)
+end
+
+z = fx(1, 2, 3, 4)
+
+println(z)
+
+
+function zeros(a, b, c)
+  if a != 0
+    x1 = (-b + sqrt(complex(b^2 - 4*a*c))) / (2*a)
+    x2 = (-b - sqrt(complex(b^2 - 4*a*c))) / (2*a)
+    
+    return [x1 x2]
+  else
+
+    if b != 0
+      return -c/b
+    else
+      return "Error"
+    end
+  end
+end
+
+zero_func = zeros(1, 2, 3)
+
+println(zero_func)
+
+return
+
--- main.jl
+++ main.jl
@@ -1,1 +1,3 @@
 include("1/main.jl")
+
+exit()
