0ea92fd3070a10acebf202627cca1b9aed2c5bd3
JoabFelippx <80433162+JoabFelippx@users.noreply.github.com>
2024-09-01 12:57:40 -0300

    bug fix

--- 4/main.jl
+++ 4/main.jl
@@ -11,22 +11,6 @@ function fixedPointMethod(f, x0, tol, maxIter)
     
 end
 
-#exercicio 3.3.1
-# exp(x) = x + 2
-# f(x) = exp(x) - x - 2
-# g(x) = exp(x) - 2
-# x_(1) = -1.8
-# tol = 10^(-8)
-
-println("Exercicio 3.3.1")
-g(x) = exp(x) - 2 # g(x) = x
-x0 = -1.8 # chute inicial
-tol = 10^(-8) # definir uma tolerancia
-maxIter = 1000 # definir um valor maximo de iteracoes
-
-x = fixedPointMethod(g, x0, tol, maxIter)
-println("x = ", x,'\n') # resposta esperada: x = -1.8414057
-
 #metodo de newton-raphson
 # formula: x_n+1 = x_n - f(x_n)/f'(x_n)
 
@@ -43,21 +27,6 @@ function newtonRaphsonMethod(f, df, x0, tol, maxIter)
     end
     return x
 end
-
-#exercicio 3.4.1
-# f(x) = cos(x) - x^2
-# f'(x) = -sin(x) - 2x
-# x_(1) = 1
-# tol = 10^(-5)
-
-println("Exercicio 3.4.1")
-f_newtonRaphson(x) = cos(x) - x^2
-df_newtonRaphson(x) = -sin(x) - 2x
-x0 = 1
-tol = 10^(-5)
-maxIter = 1000
-x = newtonRaphsonMethod(f_newtonRaphson, df_newtonRaphson, x0, tol, maxIter, )
-println("x = ", x, '\n') # resposta esperada: x = 0.82413
 
 #metodo da secante
 # formula: x_n+1 = x_n - f(x_n)*(x_n - x_n-1)/(f(x_n) - f(x_n-1))
@@ -77,21 +46,38 @@ function secantMethod(f, x0, x1, tol, maxIter)
     return x
 end
 
+maxIter = 1000 # definir um valor maximo de iteracoes
+
+#exercicio 3.3.1
+
+println("Exercicio 3.3.1")
+g(x) = exp(x) - 2 # g(x) = x
+x0 = -1.8 # chute inicial
+tol = 10^(-8) # definir uma tolerancia
+
+x = fixedPointMethod(g, x0, tol, maxIter)
+println("x = ", x,'\n') # resposta esperada: x = -1.8414057
+
+#exercicio 3.4.1
+println("Exercicio 3.4.1")
+f_newtonRaphson(x) = cos(x) - x^2
+df_newtonRaphson(x) = -sin(x) - 2x
+x0 = 1
+tol = 10^(-5)
+
+x = newtonRaphsonMethod(f_newtonRaphson, df_newtonRaphson, x0, tol, maxIter, )
+println("x = ", x, '\n') # resposta esperada: x = 0.82413
+
 #exercicio 3.6.3
-# exp(-x^2) = 2x
-# f(x) = exp(-x^2) - 2x
-# x_(1) = 0.5
-# x_(2) = 1
-# tol = 10^(-5)
 
 println("Exercicio 3.6.3")
-f_secante(x) = exp(-x^2) - 2x
+f_secante(x_fsec) = exp(-x_fsec^2) - 2(x_fsec)
 x0 = 0.5
 x1 = 1
 tol = 10^(-5)
-maxIter = 1000
-x = secantMethod(f_secante, x0, x1, tol, maxIter)
-println("x = ", x, '\n') 
+
+sect = secantMethod(f_secante, x0, x1, tol, maxIter)
+println("x = ", sect, '\n') 
 
 # exercicio 3.2.8 usando newton-raphson e secante
 println("3.2.8 - exercício")
@@ -99,27 +85,102 @@ println("3.2.8 - exercício")
 I_r = 10^-12
 V_t = ((1.38*10^-23)*(300))/(1.60*10^-19)
 tol = 1e-3
-
-#3.2.8 - a)
-R = 10^3
-V = 30
-f_newtonRaphson_2(x) = R*(I_r*(exp(x/V_t) - 1)) + x - V
-df_newtonRaphson_2(x) = R*(I_r/V_t)*exp(x/V_t) + 1
-println("a)")
-
-x0 = 0.5
 maxIter = 1000
 
+#3.2.8 - a)
+R = 1e3
+V = 30
+f_newtonRaphson_2(x_2) = R*(I_r*(exp(x_2/V_t) - 1)) + x_2 - V
+df_newtonRaphson_2(x_2) = R*(I_r/V_t)*exp(x_2/V_t) + 1
+println("a)")
+
+x0 = 0.6
+x1 = 0.7
+
 x_newtonMth = newtonRaphsonMethod(f_newtonRaphson_2, df_newtonRaphson_2, x0, tol, maxIter)
-x_secantMth = secantMethod(f_newtonRaphson_2, 0, 1, tol, maxIter)
+x_secantMth = secantMethod(f_newtonRaphson_2, x0, x1, tol, maxIter)
 println("Newton-Raphson: x = ", x_newtonMth)
 println("Secante: x = ", x_secantMth, '\n') 
 
 #3.2.8 - b)
-R = 10^3
+R = 1e3
 V = 3
+
+x0 = 0.5
+x1 = 0.6
+
 println("b)")
 x_newtonMth = newtonRaphsonMethod(f_newtonRaphson_2, df_newtonRaphson_2, x0, tol, maxIter)
-x_secantMth = secantMethod(f_newtonRaphson_2, 0, 1, tol, maxIter)
+x_secantMth = secantMethod(f_newtonRaphson_2, x0, x1, tol, maxIter)
 println("Newton-Raphson: x = ", x_newtonMth)
 println("Secante: x = ", x_secantMth, '\n') 
+
+#3.2.8 - c)
+R = 1e4
+V = 3
+
+x0 = 0.45  
+x1 = 0.55
+
+x_newtonMth = newtonRaphsonMethod(f_newtonRaphson_2, df_newtonRaphson_2, x0, tol, maxIter)
+x_secantMth = secantMethod(f_newtonRaphson_2, x0, x1, tol, maxIter)
+
+println("c)")
+println("Newton-Raphson: x = ", x_newtonMth)
+println("Secante: x = ", x_secantMth, '\n')
+
+#3.2.8 - d)
+R = 1e3
+V = 300e-3
+
+x0 = 0.2
+x1 = 0.4
+
+x_newtonMth = newtonRaphsonMethod(f_newtonRaphson_2, df_newtonRaphson_2, x0, tol, maxIter)
+x_secantMth = secantMethod(f_newtonRaphson_2, x0, x1, tol, maxIter)
+
+println("d)")
+println("Newton-Raphson: x = ", x_newtonMth)
+println("Secante: x = ", x_secantMth, '\n')
+
+#3.2.8 - e)
+R = 1e3
+V = -300e-3
+
+x0 = -0.4
+x1 = -0.2
+
+x_newtonMth = newtonRaphsonMethod(f_newtonRaphson_2, df_newtonRaphson_2, x0, tol, maxIter)
+x_secantMth = secantMethod(f_newtonRaphson_2, x0, x1, tol, maxIter)
+
+println("e)")
+println("Newton-Raphson: x = ", x_newtonMth)
+println("Secante: x = ", x_secantMth, '\n')
+
+#3.2.8 - f)
+R = 1e3
+V = -30
+
+x0 = -30.1
+x1 = -30
+
+x_newtonMth = newtonRaphsonMethod(f_newtonRaphson_2, df_newtonRaphson_2, x0, tol, maxIter)
+x_secantMth = secantMethod(f_newtonRaphson_2, x0, x1, tol, maxIter)
+
+println("f)")
+println("Newton-Raphson: x = ", x_newtonMth)
+println("Secante: x = ", x_secantMth, '\n')
+
+#3.2.8 - g)
+R = 1e4
+V = -30
+
+x0 = -30.1
+x1 = -30
+
+x_newtonMth = newtonRaphsonMethod(f_newtonRaphson_2, df_newtonRaphson_2, x0, tol, maxIter)
+x_secantMth = secantMethod(f_newtonRaphson_2, x0, x1, tol, maxIter)
+
+println("g)")
+println("Newton-Raphson: x = ", x_newtonMth)
+println("Secante: x = ", x_secantMth, '\n')
